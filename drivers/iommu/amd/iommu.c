c95731c683f1e13e61fd103bc95c7912e27c85e9
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2022-09-01 19:11:19 -0500

    iommu/amd: Introduce Extended Interrupt Remapping Table support
    The HW vIOMMU generate two types of interrupts:
       1. General and Event Log interrupt
       2. PPR Log interrupt
    
    These interrupts are virtualized using the Extended Interrupt Remapping
    Table (Extended IRT). The table is indexed by interrupt type and guest ID.
    The entry (IRTE) is programed with guest mode = 1, and leverage IOMMU AVIC
    to inject interrupt directly into the guest.
    
    Please see the AMD I/O Virtualization Technology (IOMMU) Specification
    for more detail.
    
    Link: https://www.amd.com/content/dam/amd/en/documents/processor-tech-docs/ specifications/48882_IOMMU.pdf
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -3373,14 +3373,6 @@ static int set_remap_table_entry_alias(struct pci_dev *pdev, u16 alias,
 	return 0;
 }
 
-static inline size_t get_irq_table_size(unsigned int max_irqs)
-{
-	if (!AMD_IOMMU_GUEST_IR_GA(amd_iommu_guest_ir))
-		return max_irqs * sizeof(u32);
-
-	return max_irqs * (sizeof(u64) * 2);
-}
-
 static struct irq_remap_table *alloc_irq_table(struct amd_iommu *iommu,
 					       u16 devid, struct pci_dev *pdev,
 					       unsigned int max_irqs)
@@ -4043,6 +4035,11 @@ int amd_iommu_deactivate_guest_mode(void *data)
 	entry->lo.val = 0;
 	entry->hi.val = 0;
 
+//SURAVEE: HACK
+	if (ir_data->is_ext) {
+		return 0;
+	}
+
 	entry->lo.fields_remap.valid       = valid;
 	entry->lo.fields_remap.dm          = apic->dest_mode_logical;
 	entry->lo.fields_remap.int_type    = APIC_DELIVERY_MODE_FIXED;
@@ -4196,7 +4193,12 @@ int amd_iommu_create_irq_domain(struct amd_iommu *iommu)
 int amd_iommu_update_ga(int cpu, bool is_run, void *data)
 {
 	struct amd_ir_data *ir_data = (struct amd_ir_data *)data;
-	struct irte_ga *entry = (struct irte_ga *) ir_data->entry;
+	struct irte_ga *entry;
+
+	if (!ir_data || !ir_data->entry)
+		return 0;
+
+	entry = (struct irte_ga *) ir_data->entry;
 
 	if (!AMD_IOMMU_GUEST_IR_VAPIC(amd_iommu_guest_ir) ||
 	    !entry || !entry->lo.fields_vapic.guest_mode)
