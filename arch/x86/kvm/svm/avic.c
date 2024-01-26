c8cf097c8b48458670f5e73d2621def2bdc14c16
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-01-26 03:54:56 -0600

    KVM: SVM: Introduce struct amd_iommu_svm_ops
    In order to support SVM operations needed by IOMMU driver.
    
    This is a preparation for adding extended interrupt remapping support.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- arch/x86/kvm/svm/avic.c
+++ arch/x86/kvm/svm/avic.c
@@ -1186,6 +1186,11 @@ void avic_vcpu_unblocking(struct kvm_vcpu *vcpu)
 	avic_vcpu_load(vcpu, vcpu->cpu);
 }
 
+
+const struct amd_iommu_svm_ops svm_ops = {
+	.ga_log_notifier = avic_ga_log_notifier,
+};
+
 /*
  * Note:
  * - The module param avic enable both xAPIC and x2APIC mode.
@@ -1228,7 +1233,7 @@ bool avic_hardware_setup(void)
 	if (x2avic_enabled)
 		pr_info("x2AVIC enabled\n");
 
-	amd_iommu_register_ga_log_notifier(&avic_ga_log_notifier);
+	amd_iommu_register_svm_ops(&svm_ops);
 
 	return true;
 }
