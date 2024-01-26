9e2ae94b4bc72a96730d6a74a47bed067bc4da2a
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-01-26 03:54:56 -0600

    KVM: SVM: Introduce struct amd_iommu_svm_ops
    In order to support SVM operations needed by IOMMU driver.
    
    This is a preparation for adding extended interrupt remapping support.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -40,6 +40,7 @@ static inline void amd_iommu_debugfs_setup(void) {}
 extern bool amd_iommu_viommu;
 extern const struct pt_iommu_driver_ops amd_hw_driver_ops_v1;
 extern const struct iommu_domain_ops amdv1_ops;
+extern const struct amd_iommu_svm_ops *svm_ops;
 
 /* Needed for interrupt remapping */
 int amd_iommu_prepare(void);
