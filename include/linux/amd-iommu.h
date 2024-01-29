93a43943766ab3d3b600d5717fded2ba7aed1cb1
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-01-29 00:10:09 -0600

    KVM: SVM: Introduce helper function to setup extended IRTE affinity
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- include/linux/amd-iommu.h
+++ include/linux/amd-iommu.h
@@ -20,6 +20,7 @@ struct amd_iommu_svm_ops {
 	int (*ga_log_notifier)(u32 ga_tag);
 	u32 (*get_ga_tag)(struct kvm *kvm, u32 vcpu_id);
 	u64 (*get_apic_backing_page)(struct kvm *kvm, u32 vcpu_id);
+	int (*set_ext_ir_affinity)(struct kvm *kvm, u32 vcpu_id, struct amd_iommu_pi_data *pi);
 	struct kvm *(*kvm_from_fd)(u32 kvmfd, struct fd *f);
 };
 
