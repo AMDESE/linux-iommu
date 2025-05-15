c3d4c69d1bac448f7688cacf49254564bcbbd9af
Vasant Hegde <vasant.hegde@amd.com>
2025-05-15 01:09:23 +0000

    iommu/amd: Introduce amd_iommu_sviommu_supported()
    Add support for detecting Secure vIOMMU capability in AMD IOMMUs.
    
    Introduce amd_iommu_sviommu_supported(), a new exported function that
    performs a system-wide capability check and returns whether Secure vIOMMU
    is supported across all IOMMUs in the system.
    
    Co-developed-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- include/linux/amd-iommu.h
+++ include/linux/amd-iommu.h
@@ -84,9 +84,11 @@ struct amd_iommu *get_amd_iommu(unsigned int idx);
 #ifdef CONFIG_KVM_AMD_SEV
 int amd_iommu_snp_disable(void);
 extern bool amd_iommu_sev_tio_supported(void);
+bool amd_iommu_sviommu_supported(void);
 #else
 static inline int amd_iommu_snp_disable(void) { return 0; }
 static inline bool amd_iommu_sev_tio_supported(void) { return false; }
+static inline bool amd_iommu_sviommu_supported(void) { return false; }
 #endif
 
 int amd_iommu_tmpm_enable(void);
