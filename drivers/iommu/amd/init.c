82c4153ddd4a4f9cc5f09968c2bb1e6e02b723aa
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2025-10-22 18:32:38 +0000

    iommu/amd: Map vIOMMU VF MMIO and VF Control MMIO regions
    [ Upstream commit 7d2faaf833ba ]
    
    To enable the vIOMMU feature, the AMD IOMMU driver needs to map
    vIOMMU VF MMIO and VF Control MMIO regions using information in
    the PCI vendor-specific capability (VSC).
    
    The VF Control MMIO region represents the 1st 4K of guest IOMMU mmio
    region, which contains IOMMU control registers, and will be trapped
    and handled by QEMU.
    
    The VF MMIO region represents the 3rd 4K of guest IOMMU mmio region,
    which will be virtualized by the IOMMU hardware.
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -483,7 +483,7 @@ static void iommu_disable(struct amd_iommu *iommu)
  * mapping and unmapping functions for the IOMMU MMIO space. Each AMD IOMMU in
  * the system has one.
  */
-static u8 __iomem * __init iommu_map_mmio_space(u64 address, u64 end)
+u8 __iomem * __init iommu_map_mmio_space(u64 address, u64 end)
 {
 	if (!request_mem_region(address, end, "amd_iommu")) {
 		pr_err("Can not reserve memory region %llx-%llx for mmio\n",
@@ -495,7 +495,7 @@ static u8 __iomem * __init iommu_map_mmio_space(u64 address, u64 end)
 	return (u8 __iomem *)ioremap(address, end);
 }
 
-static void __init iommu_unmap_mmio_space(struct amd_iommu *iommu)
+void __init iommu_unmap_mmio_space(struct amd_iommu *iommu)
 {
 	if (iommu->mmio_base)
 		iounmap(iommu->mmio_base);
