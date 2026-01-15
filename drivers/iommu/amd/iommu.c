f17b5cc01fc08f931e10ea779be3acc983e44738
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2026-01-15 06:08:03 +0000

    iommu/amd: Rename DEV_DOMID_MASK to DTE_DOMID_MASK
    [ Upstream commit 5335fc165749 ]
    
    Also change the define to use GENMASK_ULL instead.
    There is no functional change.
    
    Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2130,7 +2130,7 @@ static void set_dte_entry(struct amd_iommu *iommu,
 	if (dev_data->ats_enabled)
 		new.data[1] |= DTE_FLAG_IOTLB;
 
-	old_domid = READ_ONCE(dte->data[1]) & DEV_DOMID_MASK;
+	old_domid = READ_ONCE(dte->data[1]) & DTE_DOMID_MASK;
 
 	if (dev_data->tsm_enabled) {
 		/*
