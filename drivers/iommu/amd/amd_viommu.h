11a8d642c74fa4625da1812e76c66606514e93b8
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2026-03-25 07:58:17 +0000

    iommu/amd: Add helper functions to manage DevID / DomID mapping tables
    Introduce amd_viommu_init_one() and amd_viommu_uninit_one().
    These functions are called during IOMMUFD vIOMMU initialize and destroy.
    Currently, it manages the IPA mapping for Device ID and Domain ID mapping
    tables.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/amd_viommu.h
+++ drivers/iommu/amd/amd_viommu.h
@@ -14,6 +14,10 @@ void __init amd_viommu_uninit(struct amd_iommu *iommu);
 
 u64 amd_viommu_get_vfmmio_addr(struct amd_iommu *iommu, u16 gid);
 
+int amd_viommu_init_one(struct amd_iommu *iommu, struct amd_iommu_viommu *viommu);
+
+void amd_viommu_uninit_one(struct amd_iommu *iommu, struct amd_iommu_viommu *viommu);
+
 #else
 
 static inline int amd_viommu_init(struct amd_iommu *iommu)
@@ -28,6 +32,15 @@ static inline void amd_viommu_uninit(struct amd_iommu *iommu)
 static inline u64 amd_viommu_get_vfmmio_addr(struct amd_iommu *iommu, u16 gid)
 {
 	return 0;
+}
+
+static inline int amd_viommu_init_one(struct amd_iommu *iommu, struct amd_iommu_viommu *viommu)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void amd_viommu_uninit_one(struct amd_iommu *iommu, struct amd_iommu_viommu *viommu)
+{
 }
 
 #endif /* CONFIG_AMD_IOMMU_IOMMUFD */
