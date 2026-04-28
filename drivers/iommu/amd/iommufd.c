6f9cfe264353ccdf48d58084e7b60993f2401618
Vasant Hegde <vasant.hegde@amd.com>
2026-04-28 19:08:08 +0530

    iommu/amd: Enhance viommu init API to include viommu deviceID
    Host has to pass viommu deviceID to PSP during GUEST_VIOMMU_INIT call.
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>

--- drivers/iommu/amd/iommufd.c
+++ drivers/iommu/amd/iommufd.c
@@ -180,6 +180,7 @@ int amd_iommufd_viommu_init(struct iommufd_viommu *viommu, struct iommu_domain *
 
 	aviommu->trans_devid = trans_devid;
 	aviommu->trans_domid = pdom->id;
+	aviommu->viommu_devid = data.viommu_devid;
 	aviommu->kvmfd = data.kvmfd;
 	viommu->ops = &amd_viommu_ops;
 
