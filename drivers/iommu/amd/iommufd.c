18abd1a5913a72b45cd7e3d97a65bb1c9ce44524
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2026-05-26 09:15:07 +0000

    iommu/amd: Introduce Reset vMMIO Command
    Introduce new IOMMU commands for vIOMMU to reset
    virtualized MMIO registers of a particular guest.
    
    Reviewed-by: Weinan Liu <wnliu@google.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- drivers/iommu/amd/iommufd.c
+++ drivers/iommu/amd/iommufd.c
@@ -80,6 +80,9 @@ int amd_iommufd_viommu_init(struct iommufd_viommu *viommu, struct iommu_domain *
 
 	aviommu->vfmmio_mmap_offset = data.out_vfmmio_mmap_offset;
 
+	/* Reset vIOMMU MMIOs to initialize the vIOMMU */
+	iommu_reset_vmmio(iommu, aviommu->gid);
+
 	ret = iommu_copy_struct_to_user(user_data, &data,
 					IOMMU_VIOMMU_TYPE_AMD,
 					reserved);
